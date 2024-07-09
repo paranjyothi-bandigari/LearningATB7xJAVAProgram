4273b845bdb08124bc892742e5060083cdf23e93
Nagarjuna Koppala <nagarjunakoppala@Nagarjunas-Air.lan>
2024-07-09 12:57:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Java Programs
+Author - Paranjyothi
+
+This repo contains the source code for the exercise that we have done in ATB7x batch
+
+-Core Java
+-OOPs
+-Collection framework
+-Multi Threading
+-Design Patterns
+-FILE IO
+-Other Misc concepts
+-Exception handling
---
+++ src/ex_07072024/Lab002.java
@@ -0,0 +1,15 @@
+package ex_07072024;
+
+
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(3/2);
+
+
+    }
+}
