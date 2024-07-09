bf681d1c81b4ca8c0bc5d68fb56f4930424619c5
paranjyothi-bandigari <paranjyothi1994@gmail.com>
2024-07-09 13:45:27 -0600

    new file added

---
+++ src/ex_07072024/Lab003.java
@@ -0,0 +1,15 @@
+package ex_07072024;
+
+public class Lab003 {
+
+        public static void main(String[] args) {
+            System.out.println("this is my 3rd program");
+            System.out.println("Single and multi line comment");
+            //single line comment
+        /* multi
+        line
+        comment
+         */
+        }
+
+}
